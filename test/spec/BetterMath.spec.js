eb97ee248c2daec157f0eb2e3328a345b1d70066
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2012-08-18 10:21:59 -0400

    Added multiply specs

--- test/spec/BetterMath.spec.js
+++ test/spec/BetterMath.spec.js
@@ -19,4 +19,26 @@ describe('BetterMath',function(){
 		});
 	});
 
+	describe('multiply',function(){
+		it('should multiply two numbers',function(){
+			var bm = new BetterMath();
+
+			expect( bm.multiply( 2 , 2 ) ).toEqual( 4 );
+			expect( bm.multiply( 10 , 10 ) ).toEqual( 100 );
+		});
+
+		it('should produce a negative result if one operand is negative',function(){
+			var bm = new BetterMath();
+
+			expect( bm.multiply( -2 , 2 ) ).toBeLessThan( 0 );
+		});
+
+		it('should produce a positive result if both operands are negative',function(){
+			var bm = new BetterMath();
+
+			expect( bm.multiply( -2 , -2 ) ).toBeGreaterThan( 0 );
+		});
+
+	});
+
 });
