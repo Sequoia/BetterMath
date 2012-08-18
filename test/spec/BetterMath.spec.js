34065faa99ed38c86f9031d1fa211fd3eaa7b6cb
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2012-08-18 09:42:37 -0400

    Added spec to check rounding in add()

--- test/spec/BetterMath.spec.js
+++ test/spec/BetterMath.spec.js
@@ -9,6 +9,14 @@ describe('BetterMath',function(){
 			expect( bm.add( 500000000 , 2 ) ).toEqual( 500000002 );
 
 		});
+		it('should round the sum',function(){
+			var bm = new BetterMath();
+
+			expect( bm.add( 1.3 , 2.3 ) ).toEqual( 4 );
+			expect( bm.add( 0.1  , 0.1 ) ).toEqual( 0 );
+			expect( bm.add( .4, .4 ).not.toEqual(.8);
+
+		});
 	});
 
 });
