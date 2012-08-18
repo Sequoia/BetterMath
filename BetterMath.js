5bd7843029ac54f9cb2c346eaddeef22ab28c7c0
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2012-08-18 09:16:41 -0400

    calculator with add, no tests yet

---
+++ BetterMath.js
@@ -0,0 +1,14 @@
+//relies on jQuery
+function BetterMath(){
+}
+
+BetterMath.prototype.add = function( numberA, numberB ){
+	var result = this.sum( numberA, numberB );
+	return result;
+}
+
+BetterMath.prototype.sum = function( a, b ){
+	var product = a + b;
+	product = Math.round( product ); //Downstream service needs numbers rounded
+	return product;
+}
